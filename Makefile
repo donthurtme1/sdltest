4fc89fbfaeac453aada601dc592044d7d45f9f49
donthurtme1 <malwarecunt@proton.me>
2025-03-30 03:28:12 +0100

    Implemented per fragment shading

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 make:
-	sth -o include/shaders.h shader/vertex.glsl shader/fragment.glsl
+	sth -o include/shaders.h perfragshader/vertex.glsl perfragshader/fragment.glsl
 	gcc -o sdl -g src/sdl.c -Iinclude -lSDL2 -lGL -lglut -lopenblas -lm
 install: make
 	cp -f sdl /usr/local/bin/sdl
--- include/shaders.h
+++ include/shaders.h
@@ -1,53 +1,79 @@
 #ifndef INCLUDE_SHADERS_H
 #define INCLUDE_SHADERS_H
 
-const char *const shader_vertex_glsl = "\
+const char *const perfragshader_vertex_glsl = "\
 #version 460\n\
 \n\
 /* Structures */\n\
 struct CameraStruct {\n\
-	mat4 viewproj;\n\
+	mat4 mViewProj;\n\
+	vec3 pos;\n\
+};\n\
+\n\
+/* Uniforms */\n\
+layout(binding = 0) uniform Transform {\n\
+	mat4 mTransform;\n\
+};\n\
+layout(binding = 1) uniform Camera {\n\
+	CameraStruct cam;\n\
+};\n\
+\n\
+/* I/O variables */\n\
+layout(location = 0) in vec3 in_pos;\n\
+layout(location = 1) in vec3 in_normal;\n\
+\n\
+layout(location = 0) smooth out vec3 out_tpos; /* transformed pos */\n\
+layout(location = 1) smooth out vec3 out_tnormal; /* transformed normal */\n\
+\n\
+void main() {\n\
+	/* Output transformed vertex position */\n\
+	vec4 tpos = cam.mViewProj * (mTransform * vec4(in_pos, 1.0f));\n\
+	gl_Position = tpos;\n\
+	out_tpos = tpos.xyz / tpos.w;\n\
+\n\
+	/* Output transformed normal vector */\n\
+	out_tnormal = normalize((mTransform * vec4(in_normal, 0.0f)).xyz);\n\
+	//out_tnormal = tnormal.xyz;\n\
+}\n\
+";
+
+const char *const perfragshader_fragment_glsl = "\
+#version 460\n\
+\n\
+/* Structures */\n\
+struct CameraStruct {\n\
+	mat4 mViewProj;\n\
 	vec3 pos;\n\
 };\n\
 struct PointLightStruct {\n\
 	vec3 pos;\n\
-	vec3 colour;\n\
+	vec3 colour; /* intensity */\n\
 	float falloff;\n\
 };\n\
 struct MaterialStruct {\n\
-	vec3 diff_colour;\n\
-	vec3 spec_colour;\n\
+	vec3 diffuse;\n\
+	vec3 specular;\n\
 	float roughness;\n\
 };\n\
-\n\
-/* In variables */\n\
-layout(location = 0) in vec3 in_pos;\n\
-layout(location = 1) in vec3 in_normal;\n\
 \n\
 /* Uniforms */\n\
 layout(binding = 0) uniform Transform {\n\
-	mat4 mat_transform;\n\
+	mat4 mTransform;\n\
 };\n\
-\n\
 layout(binding = 1) uniform Camera {\n\
-	mat4 mat_viewproj;\n\
-	vec3 cam_pos;\n\
+	CameraStruct cam;\n\
 };\n\
-\n\
 layout(std140, binding = 2) uniform PointLightData {\n\
-	vec3 pl_pos;\n\
-	vec3 pl_colour; /* intensity */\n\
-	float pl_falloff;\n\
+	PointLightStruct pl;\n\
 };\n\
-\n\
 layout(std140, binding = 3) uniform MaterialData {\n\
-	vec3 diff_colour;\n\
-	vec3 spec_colour;\n\
-	float roughness;\n\
+	MaterialStruct mtr;\n\
 };\n\
 \n\
-/* Out variables */\n\
-layout(location = 0) smooth out vec3 out_vcolour;\n\
+/* I/O variables */\n\
+layout(location = 0) in vec3 in_tpos; /* transformed vertex position */\n\
+layout(location = 1) in vec3 in_tnormal; /* transformed normal */\n\
+out vec4 out_colour;\n\
 \n\
 /* Functions */\n\
 vec3 blinnphong(in vec3 n, in vec3 lightdir, in vec3 viewdir, in vec3 light_irradiance, in vec3 diffuse_colour, in vec3 specular_colour, in float roughness) {\n\
@@ -65,32 +91,18 @@ vec3 lightfalloff(in vec3 colour, in float falloff, in vec3 light_pos, in vec3 s
 	return colour / (r * r * falloff);\n\
 }\n\
 \n\
-void main() {\n\
-	vec4 t_pos = mat_viewproj * (mat_transform * vec4(in_pos, 1.0f));\n\
-	gl_Position = t_pos;\n\
-\n\
-	/* Transform the normal */\n\
-	vec3 t_normal = normalize((mat_transform * vec4(in_normal, 0.0f)).xyz);\n\
-	vec3 unit_pos = t_pos.xyz / t_pos.w;\n\
-	vec3 view_dir =  normalize(cam_pos - unit_pos);\n\
-	vec3 light_dir = normalize(pl_pos - unit_pos);\n\
-\n\
-	/* Calculate falloff */\n\
-	vec3 light_irradiance = lightfalloff(pl_colour, pl_falloff, pl_pos, unit_pos);\n\
-	out_vcolour = blinnphong(t_normal, light_dir, view_dir, light_irradiance, diff_colour, spec_colour, roughness);\n\
-}\n\
-";
-
-const char *const shader_fragment_glsl = "\
-#version 460\n\
-\n\
-layout(location = 0) in vec3 colour;\n\
-\n\
-out vec4 fragcolour;\n\
-\n\
 void\n\
 main() {\n\
-	fragcolour = vec4(colour, 1.0f);\n\
+	/* Transform the normal */\n\
+	//vec3 t_normal = normalize((mTransform * vec4(in_tnormal, 0.0f)).xyz);\n\
+	//vec3 unit_pos = in_tpos.xyz;\n\
+	vec3 view_dir =  normalize(cam.pos - in_tpos);\n\
+	vec3 light_dir = normalize(pl.pos - in_tpos);\n\
+\n\
+	/* Calculate falloff */\n\
+	vec3 light_irradiance = lightfalloff(pl.colour, pl.falloff, pl.pos, in_tpos);\n\
+	vec3 colour = blinnphong(in_tnormal, light_dir, view_dir, light_irradiance, mtr.diffuse, mtr.specular, mtr.roughness);\n\
+	out_colour = vec4(colour, 1.0f);\n\
 }\n\
 ";
 
